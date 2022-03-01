0d490df1799bda8af2ca07b091ccb9a770228935
PabloCed <pablolara25@aragon.unam.mx>
2022-03-01 13:04:54 -0600

    Sesion 1 marzo, Automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author PC
@@ -16,6 +20,21 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
-    }
+        Silla sl = new Silla();
+        System.out.println(sl); //por defecto se importa toString()
+        System.out.println(sl.toString());
 
+        Automovil miBocho = new Automovil();
+        System.out.println(miBocho);
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setColor(Color.BLUE);
+        System.out.println(miBocho);
+
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustang);
+        Automovil miAkura = new Automovil("Akura", "NSX", 2013, Color.gray);
+        System.out.println(miAkura);
+
+    }
 }
